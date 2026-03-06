6b26f5c0427f50b5b815e225d720349af0815f34
LyMeng12 <mengsaoly@gmail.com>
2026-03-06 15:35:41 +0700

    update subject into teacher

--- src/main/java/com/systemSchool/School/api/Model/SubjectAPI.java
+++ src/main/java/com/systemSchool/School/api/Model/SubjectAPI.java
@@ -3,20 +3,24 @@ package com.systemSchool.School.api.Model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
+@ToString
 @Entity
 @Table(name="Subject")
 public class SubjectAPI {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long subjectId;
-    @Column(unique=true, nullable=false)
+    private Long id;
+
     private String subjectName;
 
-    @OneToMany
-    private List<TeacherAPI> teacherAPI;
+
+    @OneToOne
+    @JoinColumn(name = "teacher_id")
+    private TeacherAPI teacher;
 }
--- src/main/java/com/systemSchool/School/api/Model/TeacherAPI.java
+++ src/main/java/com/systemSchool/School/api/Model/TeacherAPI.java
@@ -19,6 +19,8 @@ public class TeacherAPI {
     private String teacherName;
     private String teacherSubject;
     private Double salary;
+    @OneToOne(mappedBy = "teacher")
+    private SubjectAPI subject;
 
     @ManyToMany
     @JoinTable(
